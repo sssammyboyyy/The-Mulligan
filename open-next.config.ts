6ccb6b711160cdcf8f5962bae48da32fff9da776
sssammyboyyy <131851932+sssammyboyyy@users.noreply.github.com>
2025-11-20 14:08:22 +0200

    Add initial Open Next configuration file

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "@opennextjs/cloudflare";
+
+export default defineConfig({
+  // For most Next.js apps, this default works.
+  // You can customize this later if needed.
+});
